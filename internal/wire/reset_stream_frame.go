fe524ac9de6517efb1d4eb676f315826ceeb1a3d
Marten Seemann <martenseemann@gmail.com>
2022-11-04 14:16:29 +0000

    implement parsing and writing of RELIABLE_RESET_STREAM frames

--- internal/wire/reset_stream_frame.go
+++ internal/wire/reset_stream_frame.go
@@ -2,31 +2,32 @@ package wire
 
 import (
 	"bytes"
+	"fmt"
 
 	"github.com/quic-go/quic-go/internal/protocol"
 	"github.com/quic-go/quic-go/internal/qerr"
 	"github.com/quic-go/quic-go/quicvarint"
 )
 
-// A ResetStreamFrame is a RESET_STREAM frame in QUIC
+// A ResetStreamFrame is a RESET_STREAM or RELIABLE_RESET_STREAM frame in QUIC
 type ResetStreamFrame struct {
-	StreamID  protocol.StreamID
-	ErrorCode qerr.StreamErrorCode
-	FinalSize protocol.ByteCount
+	StreamID     protocol.StreamID
+	ErrorCode    qerr.StreamErrorCode
+	FinalSize    protocol.ByteCount
+	ReliableSize protocol.ByteCount
 }
 
 func parseResetStreamFrame(r *bytes.Reader, _ protocol.VersionNumber) (*ResetStreamFrame, error) {
-	if _, err := r.ReadByte(); err != nil { // read the TypeByte
+	typ, err := r.ReadByte()
+	if err != nil { // read the TypeByte
 		return nil, err
 	}
 
-	var streamID protocol.StreamID
-	var byteOffset protocol.ByteCount
 	sid, err := quicvarint.Read(r)
 	if err != nil {
 		return nil, err
 	}
-	streamID = protocol.StreamID(sid)
+	streamID := protocol.StreamID(sid)
 	errorCode, err := quicvarint.Read(r)
 	if err != nil {
 		return nil, err
@@ -35,20 +36,40 @@ func parseResetStreamFrame(r *bytes.Reader, _ protocol.VersionNumber) (*ResetStr
 	if err != nil {
 		return nil, err
 	}
-	byteOffset = protocol.ByteCount(bo)
+	finalSize := protocol.ByteCount(bo)
+
+	var reliableSize protocol.ByteCount
+	if typ == 0x72 {
+		rs, err := quicvarint.Read(r)
+		if err != nil {
+			return nil, err
+		}
+		reliableSize = protocol.ByteCount(rs)
+	}
+	if reliableSize > finalSize {
+		return nil, fmt.Errorf("RELIABLE_RESET_STREAM: reliable size can't be larger than final size (%d vs %d)", reliableSize, finalSize)
+	}
 
 	return &ResetStreamFrame{
-		StreamID:  streamID,
-		ErrorCode: qerr.StreamErrorCode(errorCode),
-		FinalSize: byteOffset,
+		StreamID:     streamID,
+		ErrorCode:    qerr.StreamErrorCode(errorCode),
+		FinalSize:    finalSize,
+		ReliableSize: reliableSize,
 	}, nil
 }
 
 func (f *ResetStreamFrame) Append(b []byte, _ protocol.VersionNumber) ([]byte, error) {
-	b = append(b, 0x4)
+	typ := uint64(0x4)
+	if f.ReliableSize > 0 {
+		typ = 0x72
+	}
+	b = quicvarint.Append(b, typ)
 	b = quicvarint.Append(b, uint64(f.StreamID))
 	b = quicvarint.Append(b, uint64(f.ErrorCode))
 	b = quicvarint.Append(b, uint64(f.FinalSize))
+	if f.ReliableSize > 0 {
+		b = quicvarint.Append(b, uint64(f.ReliableSize))
+	}
 	return b, nil
 }
 
